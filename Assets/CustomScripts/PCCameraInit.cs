ce42da8f2c026e6a09cb14e3c8144b82126f46fb
SathR12 <sathya_regu@outlook.com>
2025-02-11 14:19:45 -0500

    Added Google Cardboard SDK support and multiple cameras to switch between windows and android builds

---
+++ Assets/CustomScripts/PCCameraInit.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraInit : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if (!Application.isEditor && Application.platform != RuntimePlatform.WindowsPlayer)
+        {
+            gameObject.SetActive(false);
+        }
+
+        
+    }
+}
