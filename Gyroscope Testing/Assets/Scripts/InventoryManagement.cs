1824faf4cd4cde11ec92a100d8b4a14dbf41d983
DefinitelyNotRelevant <davidli6543@gmail.com>
2025-03-10 12:10:31 -0400

    Inventory Bug Fixes and Code Clean-Up

--- Gyroscope Testing/Assets/Scripts/InventoryManagement.cs
+++ Gyroscope Testing/Assets/Scripts/InventoryManagement.cs
@@ -2,49 +2,72 @@ using UnityEngine;
 
 public class InventoryManagement: MonoBehaviour
 {
-    public GameObject storedItem; // Currently holds one, will figure out multiple items.
+    public GameObject storedItem;
     private int timer;
+    private Vector3 storedPosition;
+    private Vector3 storedDirection;
+    private Quaternion storedRotation;
+    private Vector3 lastCameraPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         timer = 0;
         storedItem = null;
+        lastCameraPosition = transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (transform.rotation.eulerAngles.x > 65f && transform.rotation.eulerAngles.x < 270f && storedItem?.activeSelf == false && timer > 100)
+        if (transform.rotation.eulerAngles.x >= 65f && transform.rotation.eulerAngles.x < 270f && storedItem?.activeSelf == false) // If camera is looking down
         {
-            PresentInventory();
-            timer = 0;
+            // Starts counting down for 100 frames
+            if (timer > 100) // Show inventory after 100 frames
+            {
+                PresentInventory();
+            }
+            else if (timer == 0) // Calculate spawn location on the first frame the camera looks down
+            {
+                PrepInventory();
+                timer++;
+            }
+            else // Count up this frame
+            {
+                timer++;
+            }
         }
-        else if (transform.rotation.eulerAngles.x > 65f && transform.rotation.eulerAngles.x < 270f && storedItem?.activeSelf == false)
+        else if (transform.rotation.eulerAngles.x < 65f || lastCameraPosition != transform.position) // Looking up or post teleport reset timer.
         {
-            timer++;
-        }
-        else if (transform.rotation.eulerAngles.x < 65f)
-        {
+            lastCameraPosition = transform.position;
             storedItem?.SetActive(false);
             timer = 0;
         }
     }
-
+    
+    // Prepare inventory spawn location
+    void PrepInventory()
+    {
+        storedDirection = transform.forward;
+        storedPosition = transform.position + transform.forward * (1f + Camera.main.nearClipPlane);
+        storedPosition = new Vector3(storedPosition.x, 1f, storedPosition.z); // Eventually set it to spawn the same distance away
+        storedRotation = transform.rotation;
+    }
+    
+    // Show prepared inventory
     void PresentInventory()
     {
         // Ideally it could fade in the closer you look to ground maybe
-        storedItem.transform.position = Camera.main.transform.position + Camera.main.transform.forward * (1f + Camera.main.nearClipPlane);
-        storedItem.transform.position = new Vector3(storedItem.transform.position.x, 1f, storedItem.transform.position.z); // Eventually set it to spawn the same distance away
-        storedItem.transform.rotation = Camera.main.transform.rotation;
+        storedItem.transform.position = storedPosition;
+        storedItem.transform.rotation = storedRotation;
         storedItem.transform.Rotate(90f, 0f, 0f);
         storedItem.SetActive(true);
     }
 
+    // Stores item to be shown in inventory (Add multi-item inventory later)
     public void EnterIntoInventory(GameObject item)
     {
         storedItem = item;
     }
-
     
 }
