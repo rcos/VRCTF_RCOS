b5194773d4bd795be5fa22400780487d40d33371
Ashley Liang <148646090+AshleyL119@users.noreply.github.com>
2025-06-17 15:25:19 -0400

    Switch from Sign In UI to Email UI Screen

--- Gyroscope Testing/Assets/Scripts/TestingScripts/KeyboardTestScene_Monitor.cs
+++ Gyroscope Testing/Assets/Scripts/TestingScripts/KeyboardTestScene_Monitor.cs
@@ -4,71 +4,86 @@ using TMPro;
 
 public class KeyboardTestScene_Monitor : MonoBehaviour
 {
-    [SerializeField] private String password;
+    [Header("Scenario Settings")]
+    [SerializeField] private bool isScenario2 = false; // Toggle in Inspector
+
+    [Header("References")]
+    [SerializeField] private string password;
     [SerializeField] private GameObject manager;
+    [SerializeField] private GameObject signInUI;
+    [SerializeField] private GameObject emailUI;
+
     private GameObject keyboard = null;
+
     public TextMeshPro TMP_Text;
     public TextMeshPro Status;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        // Ensure email UI is hidden initially
+        if (emailUI != null)
+            emailUI.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    // Runs when the camera is first looking at an object
-    public void OnPointerEnter()
-    {
-        
-    }
-
-    // Runs when the camera is no longer looking at an object
-    public void OnPointerExit()
-    {
-        
-    }
-
-    // Runs when the phone is tapped on while looking at an object
     public void OnPointerClick()
     {
-        if (keyboard != null) {
+        if (keyboard != null)
+        {
             Keyboard_3D_Static.destroyKeyboard(keyboard);
             keyboard = null;
         }
-        keyboard = Keyboard_3D_Static.makeNewKeyboardObjectAndKeys(GameEnums.Keyboard_Type.LowercaseOnly, 0.1f, 0.1f,
-                    (string charPressed, string fullString) => { keyPressed(charPressed, fullString); },
-                    (string fullString) => { onSubmit(fullString); },
-                    (string fullString) => { onCancel(fullString); },
-                    null
-                  );
+
+        keyboard = Keyboard_3D_Static.makeNewKeyboardObjectAndKeys(
+            GameEnums.Keyboard_Type.LowercaseOnly, 0.1f, 0.1f,
+            (charPressed, fullString) => keyPressed(charPressed, fullString),
+            (fullString) => onSubmit(fullString),
+            (fullString) => onCancel(fullString),
+            null
+        );
+
         Keyboard_3D_Static.setPosition(keyboard, new Vector3(-3.3f, 0.9f, 2.6f));
         Keyboard_3D_Static.setRotation(keyboard, new Vector3(-90f, -90f, 0f));
         Keyboard_3D_Static.setScale(keyboard, new Vector3(0.3f, 0.3f, 0.3f));
+
         TMP_Text.text = "";
     }
 
-    void keyPressed(string charPressed, string fullString) {
+    void keyPressed(string charPressed, string fullString)
+    {
         TMP_Text.text = fullString;
     }
-    void onSubmit(string fullString) {
+
+    void onSubmit(string fullString)
+    {
         Keyboard_3D_Static.setPosition(keyboard, new Vector3(0, -2000f, 3.35f));
-        if (fullString == password) {
-            Status.text = "Correct!";
-            manager.GetComponent<ScenarioManager>().FlagTriggered();
-        } else {
-            Status.text = "Incorrect!";
+
+        if (fullString == password)
+        {
+            if (isScenario2)
+            {
+                // Scenario 2: Switch from sign-in to email UI
+                if (signInUI != null) signInUI.SetActive(false);
+                if (emailUI != null) emailUI.SetActive(true);
+            }
+            else
+            {
+                // Scenario 1: Show "Correct!" and trigger flag
+                if (Status != null) Status.text = "Correct!";
+                if (manager != null) manager.GetComponent<ScenarioManager>().FlagTriggered();
+            }
         }
+        else
+        {
+            if (Status != null) Status.text = "Incorrect!";
+        }
+
         TMP_Text.text = "";
     }
-    void onCancel(string fullString) {
+
+    void onCancel(string fullString)
+    {
         Keyboard_3D_Static.setPosition(keyboard, new Vector3(0, -2000f, 3.35f));
-        Status.text = "Incorrect!";
+        if (Status != null) Status.text = "Incorrect!";
         TMP_Text.text = "";
     }
 }
