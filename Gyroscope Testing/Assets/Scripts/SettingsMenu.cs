f2d8b587493b2c28698820fc686914309cf1f641
Busaba-Nicholas <busabaN2014@verizon.net>
2025-09-23 17:21:04 -0400

    Added Gear Icon

---
+++ Gyroscope Testing/Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    private GameObject wayToOpenSettings = null;
+    private bool isSettingsOpen = false;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
