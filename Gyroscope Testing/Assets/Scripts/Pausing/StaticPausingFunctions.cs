cf55bd1c7a9a48c54ae51f91457dee9f34fb955e
Busaba-Nicholas <busabaN2014@verizon.net>
2025-10-21 17:32:04 -0400

    Functional 2 button pause menu

--- Gyroscope Testing/Assets/Scripts/Pausing/StaticPausingFunctions.cs
+++ Gyroscope Testing/Assets/Scripts/Pausing/StaticPausingFunctions.cs
@@ -3,18 +3,27 @@ using TMPro;
 
 public static class StaticPausingFunctions
 {
+    /* ---------------------------------------------------------------------- */
+    /* ---------------------------------------------------------------------- */
+    /* --------------------------- Pausing ---------------------------------- */
+    /* ---------------------------------------------------------------------- */
+    /* ---------------------------------------------------------------------- */
+
     public static Vector3 pauseAreaLocation = new Vector3(1000,1000,1000);
     public static Vector3 lastKnownPlayerPosition;
 
     public static bool allowedToPause = true;
+    public static bool currentlyUnPausing = false;
     public static bool currentlyPausing = false;
     public static bool currentlyPaused = false;
 
-    public static (string, System.Type)[] allPauseOptions = { ("Resume", typeof(PauseMenu_Resume)) };
+    public static (string, System.Type)[] allPauseOptions = { ("Resume", typeof(PauseMenu_Resume)), ("Settings", typeof(PauseMenu_Settings)) };
+    public static GameObject[] allButtonsMade = null;
 
     public static void PauseGame() {
-        if (currentlyPausing || currentlyPaused) return;
+        if (!allowedToPause || currentlyPausing || currentlyPaused) return;
         currentlyPausing = true;
+        allButtonsMade = new GameObject[allPauseOptions.Length];
 
         // setup
         GameObject player = GameObject.Find("Player");
@@ -24,22 +33,26 @@ public static class StaticPausingFunctions
             thePauseArea = Object.Instantiate(prefab);
         }
         thePauseArea.transform.position = pauseAreaLocation;
+        thePauseArea.transform.eulerAngles = new Vector3(0, Camera.main.transform.eulerAngles.y, 0);
         lastKnownPlayerPosition = player.transform.position;
+        if (settingsOpen) toggleSettings();
 
         // show all pause options
-        Transform PauseOptionsBox = thePauseArea.transform.Find("SimplePauseOptions").transform.Find("SimplePauseOptions_Background");
-        MeshRenderer PauseOptionsBox_Mesh = PauseOptionsBox.GetComponent<MeshRenderer>();
+        Transform PauseOptionsContainer = thePauseArea.transform.Find("SimplePauseOptions");
+        Transform PauseOptionsRenderer = PauseOptionsContainer.transform.Find("SimplePauseOptions_Background");
+        MeshRenderer PauseOptionsBox_Mesh = PauseOptionsRenderer.GetComponent<MeshRenderer>();
         if (PauseOptionsBox_Mesh == null) { Debug.LogError("Couldn't find an object within a prefab"); }
         float upperY = PauseOptionsBox_Mesh.bounds.max.y-1.0f; // text saying "Paused" takes up space
         float lowerY = PauseOptionsBox_Mesh.bounds.min.y+0.5f;
         float differencePerButton = (upperY - lowerY) / (allPauseOptions.Length+1);
         GameObject buttonPreFab = Resources.Load<GameObject>("Pausing/IndividualButton");
         for (int i = 0; i < allPauseOptions.Length; i++) {
-            GameObject curButton = Object.Instantiate(buttonPreFab);
-            curButton.transform.position = new Vector3(PauseOptionsBox.position.x, upperY-differencePerButton, PauseOptionsBox.position.z);
+            GameObject curButton = Object.Instantiate(buttonPreFab, PauseOptionsContainer);
+            allButtonsMade[i] = curButton;
+            curButton.transform.position = new Vector3(PauseOptionsContainer.position.x, upperY-((i+1)*differencePerButton), PauseOptionsContainer.position.z);
 
-            PauseMenu_ButtonParent logicScript = (PauseMenu_ButtonParent)curButton.AddComponent(allPauseOptions[0].Item2); // add instance of class so the object knows what function to call
-            curButton.transform.Find("Text (TMP)").GetComponent<TextMeshPro>().text = allPauseOptions[0].Item1;
+            PauseMenu_ButtonParent logicScript = (PauseMenu_ButtonParent)curButton.AddComponent(allPauseOptions[i].Item2); // add instance of class so the object knows what function to call
+            curButton.transform.Find("Text (TMP)").GetComponent<TextMeshPro>().text = allPauseOptions[i].Item1;
             curButton.transform.Find("Button").GetComponent<PauseMenuButtonScript>().scriptToCall = logicScript;
         }
 
@@ -53,7 +66,33 @@ public static class StaticPausingFunctions
     }
 
     public static void UnpauseGame() {
-        
+        if (!currentlyPaused || currentlyPausing || currentlyUnPausing) return;
+        currentlyUnPausing = true;
+
+        // make black fade in appear
+        GameObject fadeOutSquareForNewScene = null;
+        fadeOutSquareForNewScene = FadeOutSquare_Static.makeNewFadeOutSquare(10,10,10, (GameEnums.FadeOutSquare_CallbackType _) => {
+            GameObject.Find("Player").transform.position = lastKnownPlayerPosition;
+            currentlyUnPausing = false;
+            currentlyPaused = false;
+            for (int i = 0; i < allButtonsMade.Length; i++) {
+                Object.Destroy(allButtonsMade[i]);
+            }
+            allButtonsMade = null;
+        });
+    }
+
+    /* ---------------------------------------------------------------------- */
+    /* ---------------------------------------------------------------------- */
+    /* ------------------------ Settings Menu ------------------------------- */
+    /* ---------------------------------------------------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    public static bool settingsOpen = false;
+
+    public static void toggleSettings() {
+        settingsOpen = !settingsOpen;
+        GameObject.Find("PauseArea(Clone)").transform.Find("SimpleSettingsOptions").gameObject.SetActive(settingsOpen);
     }
 }
 
@@ -63,5 +102,8 @@ public class PauseMenu_ButtonParent : MonoBehaviour {
 }
 
 public class PauseMenu_Resume : PauseMenu_ButtonParent {
-    public override void Pressed() { Debug.Log("PauseMenu_Resume's function was called when pressed"); }
+    public override void Pressed() { StaticPausingFunctions.UnpauseGame(); }
+}
+public class PauseMenu_Settings : PauseMenu_ButtonParent {
+    public override void Pressed() { StaticPausingFunctions.toggleSettings(); }
 }
