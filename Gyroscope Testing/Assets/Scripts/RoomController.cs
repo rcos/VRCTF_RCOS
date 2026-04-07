fd8741769fa84ba379a9e1465fa44bb9857b1719
DefinitelyNotRelevant <davidli6543@gmail.com>
2026-04-07 16:52:13 -0400

    Implemented floor changes in Scenario 4.
    Keypad interactions move player between floor 1 and floor 2 in Scenario 4.

--- Gyroscope Testing/Assets/Scripts/RoomController.cs
+++ Gyroscope Testing/Assets/Scripts/RoomController.cs
@@ -4,6 +4,7 @@ public class RoomController : MonoBehaviour
 {
     private GameObject _player;
     public Vector3 teleportPosition;
+    public bool requiresCondition;
 
     void Start()
     {
@@ -12,7 +13,21 @@ public class RoomController : MonoBehaviour
     
     public void MovePosition()
     {
-        _player.transform.position = teleportPosition;
+        FadeOutSquare_Static.makeNewFadeOutSquare(10, 8, 10,
+            (GameEnums.FadeOutSquare_CallbackType reason) =>
+            {
+                // this is run when screen is fully black
+                _player.transform.position = teleportPosition;
+            }
+        );
         Debug.Log(_player.transform.position);
+    }
+
+    void OnPointerClick()
+    {
+        if (!requiresCondition)
+        {
+            MovePosition();
+        }
     }
 }
